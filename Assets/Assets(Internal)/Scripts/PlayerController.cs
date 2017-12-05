3aa82ebaa7b730c2c614071881dd6842538105fd
PatrikGlendell <pada.gle@gmail.com>
2017-12-05 16:04:10 +0100

    initial commit

---
+++ Assets/Assets(Internal)/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 18;
+
+    private Rigidbody playerRigid;
+    void Start()
+    {
+        playerRigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * playerSpeed * Time.deltaTime;
+
+        playerRigid.MovePosition(transform.position + movement);
+    }
+}
